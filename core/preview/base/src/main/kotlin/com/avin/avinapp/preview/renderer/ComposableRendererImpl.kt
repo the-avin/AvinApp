064c64f76d6f9917f5832b581718dfc8ac0cef30
Amirreza Gholami <jv233114@gmail.com>
2025-05-02 18:15:17 +0330

    remove: remove reflection based codes

--- core/preview/base/src/main/kotlin/com/avin/avinapp/preview/renderer/ComposableRendererImpl.kt
+++ core/preview/base/src/main/kotlin/com/avin/avinapp/preview/renderer/ComposableRendererImpl.kt
@@ -1,15 +1,11 @@
 package com.avin.avinapp.preview.renderer
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentComposer
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.InternalComposeUiApi
@@ -21,12 +17,9 @@ import androidx.compose.ui.scene.ComposeScene
 import androidx.compose.ui.unit.Density
 import com.avin.avinapp.data.models.device.PreviewDevice
 import com.avin.avinapp.data.models.device.intSize
-import com.avin.avinapp.data.models.widget.buttonDescriptor
 import com.avin.avinapp.preview.collector.ComponentRenderCollector
 import com.avin.avinapp.preview.collector.NewLocalComponentRenderCollector
 import com.avin.avinapp.preview.collector.trackRender
-import com.avin.avinapp.preview.runtime.InvokeComposableService
-import com.avin.avinapp.preview.runtime.InvokeComposableServiceImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.JsonObject
@@ -36,22 +29,12 @@ import kotlinx.serialization.json.jsonPrimitive
 
 class ComposableRendererImpl(
     private val collector: ComponentRenderCollector,
-    val invokeComposableService: InvokeComposableService = InvokeComposableServiceImpl()
 ) : ComposableRenderer {
     override fun renderComposable(json: JsonObject): @Composable () -> Unit {
         val count = json[ComposableRenderer.TYPE]?.jsonPrimitive?.contentOrNull?.toInt() ?: 1
         return {
             NewLocalComponentRenderCollector(collector) {
                 MaterialTheme {
-                    Column(Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
-                        repeat(count) {
-                            invokeComposableService.invokeCaching(
-                                it.toString(),
-                                currentComposer,
-                                buttonDescriptor
-                            )
-                        }
-                    }
                     Box(
                         Modifier.fillMaxSize(),
                         contentAlignment = Alignment.Center
--- core/preview/base/src/main/kotlin/com/avin/avinapp/preview/runtime/InvokeComposableServiceListener.kt
+++
@@ -1,5 +0,0 @@
-package com.avin.avinapp.preview.runtime
-
-interface InvokeComposableServiceListener {
-    fun onError(throwable: Throwable)
-}
